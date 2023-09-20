5aab18b0a8b6e75c47b8571b67712442ef2f65bb
siva9192 <barinepallisiva@gmail.com>
2023-09-20 16:07:20 +0530

    Added 007 Dynamically Execute Packages project

--- SSIS Real Time Scenarios/07 Dynamically Execute Packages in SSIS/01_CreateTable.sql
+++ SSIS Real Time Scenarios/07 Dynamically Execute Packages in SSIS/01_CreateTable.sql
@@ -1,14 +1,16 @@
-IF EXISTS(SELECT 1 FROM sysobjects with (nolock) WHERE ID = OBJECT_ID(N'Pkg_Details') AND type = (N'U'))
-drop table Pkg_Details
+IF EXISTS(SELECT 1 FROM sysobjects with (nolock) WHERE ID = OBJECT_ID(N'007 Dynamically Execute Packages') AND type = (N'U'))
+drop table [007 Dynamically Execute Packages]
 go
-CREATE TABLE Pkg_Details(ID INT IDENTITY, PackageFolderPath varchar(300), PackageName varchar(100), Active bit)
+CREATE TABLE [007 Dynamically Execute Packages](ID INT IDENTITY, PackageFolderPath varchar(300), PackageName varchar(100), Active bit)
 go
 --Insert package details
-INSERT INTO Pkg_Details VALUES('C:\MSBI\SSIS\Training\DynamicallyExecutePackagesinSSIS\DynamicallyExecutePackagesinSSIS',
+INSERT INTO [007 Dynamically Execute Packages] VALUES('C:\MSBI\SSIS\Training\DynamicallyExecutePackagesinSSIS\DynamicallyExecutePackagesinSSIS',
 'ChildPkg1.dtsx',1)
-INSERT INTO Pkg_Details VALUES('C:\MSBI\SSIS\Training\DynamicallyExecutePackagesinSSIS\DynamicallyExecutePackagesinSSIS',
+INSERT INTO [007 Dynamically Execute Packages] VALUES('C:\MSBI\SSIS\Training\DynamicallyExecutePackagesinSSIS\DynamicallyExecutePackagesinSSIS',
 'ChildPkg2.dtsx',1)
-INSERT INTO Pkg_Details VALUES('C:\MSBI\SSIS\Training\DynamicallyExecutePackagesinSSIS\DynamicallyExecutePackagesinSSIS',
+INSERT INTO [007 Dynamically Execute Packages] VALUES('C:\MSBI\SSIS\Training\DynamicallyExecutePackagesinSSIS\DynamicallyExecutePackagesinSSIS',
 'ChildPkg3.dtsx',1)
 go
-select PackageFolderPath, PackageName from Pkg_Details where Active=1
+select PackageFolderPath, PackageName from [007 Dynamically Execute Packages] where Active=1
+
+CREATE TABLE [007 Logs](ID INT IDENTITY, Step varchar(300), PackageName varchar(100), Dated DateTime)
