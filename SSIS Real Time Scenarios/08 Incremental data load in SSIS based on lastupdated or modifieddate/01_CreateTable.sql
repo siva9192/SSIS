f2792f709a75a7976640c432db518f38073b4819
siva9192 <barinepallisiva@gmail.com>
2023-09-26 12:58:38 +0530

    added 014 Get time taken by ssis package project

--- SSIS Real Time Scenarios/08 Incremental data load in SSIS based on lastupdated or modifieddate/01_CreateTable.sql
+++ SSIS Real Time Scenarios/08 Incremental data load in SSIS based on lastupdated or modifieddate/01_CreateTable.sql
@@ -1,2 +1,6 @@
-create table audit_log(Id int identity, PackageName varchar(200), TableName varchar(200), RecordsInserted int,
+--Create log table
+create table [008 audit_log](Id int identity, PackageName varchar(200), TableName varchar(200), RecordsInserted int,
 RecordsUpdated int, Dated datetime)
+
+--Create config table
+CREATE TABLE [008 Config_Table] (Id int identity,TableName VARCHAR(200),lastUpdatedColumn varchar(200), lastUpdatedvalue datetime)
--- SSIS Real Time Scenarios/14 Get time taken by SSIS package/01_CreateTable.sql
+++ SSIS Real Time Scenarios/14 Get time taken by SSIS package/01_CreateTable.sql
@@ -1,16 +1,18 @@
-IF OBJECT_ID('audit_log', 'U') IS NULL
-create table audit_log(Id int identity, PackageName varchar(100), StartTime datetime, EndTime datetime, TimeTakenInMin int, Status
+IF OBJECT_ID('014 audit_log', 'U') IS NULL
+create table [014 audit_log](Id int identity, PackageName varchar(100), StartTime datetime, EndTime datetime, TimeTakenInMin int, Status
 varchar(10))
-insert into audit_log
+
+
+insert into [014 audit_log]
 select 'Package.dtsx',getdate(), null, 0,''
 select @@IDENTITY
 
+WAITFOR DELAY '00:01:10'
 
-
-
+SELECT * FROM [014 audit_log]
 -----------------------------------------
 
-"update audit_log
+"update [014 audit_log]
 set EndTime = GETDATE(),
 TimeTakenInMin = datediff(minute,StartTime,getdate()),
 Status = 'Success'
